b91b46a83b3ff21603cb7d755beed8d4775677d5
Shashwat <ss44@usf.edu>
2026-03-29 16:17:03 -0400

    added fetchProductsAsync function

---
+++ cp_2.js
@@ -0,0 +1,12 @@
+function fetchProductsThen() {
+    fetch("https://www.course-api.com/javascript-store-products")
+        .then(response => response.json())
+        .then(products => {
+            products.forEach(product => {
+                console.log(product.fields.name);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching products:', error);
+        });
+}
