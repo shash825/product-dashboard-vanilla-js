74e7ceca790648b27b6bb8c283d0d24142a5c0d6
Shashwat <ss44@usf.edu>
2026-03-29 16:24:39 -0400

    fetch product data with async await and handle errors

--- cp_2.js
+++ cp_2.js
@@ -10,3 +10,13 @@ function fetchProductsThen() {
             console.error('Error fetching products:', error);
         });
 }
+
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch("https://www.course-api.com/javascript-store-products");
+        const products = await response.json();
+        displayProducts(products);
+    } catch (error) {
+        handleError(error);
+    }
+}
